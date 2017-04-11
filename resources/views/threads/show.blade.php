61a0b6d8ce2133eb2cf54f16c36d0e68cefec028
CJEdgerton <cjedgerton@gmail.com>
2017-04-10 22:09:19 -0400

    Building up Reply model, view integration, and tests.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -13,5 +13,25 @@
             </div>
         </div>
     </div>
+
+
+    <h3 class="text-center">Replies</h3>
+    <hr>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            @foreach( $thread->replies as $reply )
+                <div class="panel panel-default">
+
+                    <div class="panel-heading">
+                        <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }} ...
+                    </div>
+
+                    <div class="panel-body">
+                        <p>{{ $reply->body }}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </div>
 @endsection
