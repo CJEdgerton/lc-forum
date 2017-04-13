c6f8b548b56f29f6ec2ce1440a3fc9c5b65f1948
CJEdgerton <cjedgerton@gmail.com>
2017-04-12 21:21:43 -0400

    User may respond to thread - testing.

--- app/Reply.php
+++ app/Reply.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+	protected $guarded = [];
+
 	public function owner() 
 	{
 		return $this->belongsTo('App\User', 'user_id');
+	}
+
+	public function thread()
+	{
+		return $this->belongsTo('App\Thread');
 	}
 }
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,7 +5,10 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">{{ $thread->title }}</div>
+                <div class="panel-heading">
+                    <a href="#">{{ $thread->creator->name }}</a> posted:
+                    {{ $thread->title }}
+                </div>
 
                 <div class="panel-body">
                     {{ $thread->body }}
@@ -20,16 +23,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             @foreach( $thread->replies as $reply )
-                <div class="panel panel-default">
-
-                    <div class="panel-heading">
-                        <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }} ...
-                    </div>
-
-                    <div class="panel-body">
-                        <p>{{ $reply->body }}</p>
-                    </div>
-                </div>
+                @include('threads.reply')
             @endforeach
         </div>
     </div>
