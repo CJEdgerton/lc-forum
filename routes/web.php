ffa7c64fea8b4766dbaede0ad5d8791dc078400f
CJEdgerton <cjedgerton@gmail.com>
2017-04-15 21:16:51 -0400

    User may publish threads, and associated tests.

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/home', 'HomeController@index');
 Route::get('/threads', 'ThreadsController@index');
 Route::get('/threads/{thread}', 'ThreadsController@show');
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
+Route::post('/threads', 'ThreadsController@store');
 
