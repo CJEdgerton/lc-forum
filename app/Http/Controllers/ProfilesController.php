48e7322541d3dbb0356f3e9f21602ab1ffa96b22
CJEdgerton <cjedgerton@gmail.com>
2017-05-21 20:28:46 -0400

    Creating 'activiy feed' component in profile view.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -18,7 +18,14 @@ class ProfilesController extends Controller
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'threads'     => $user->threads()->paginate(10)
+            'activities'  => $this->getActivity($user),
         ]);
+    }
+
+    public function getActivity(User $user)
+    {
+        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function($activity) {
+            return $activity->created_at->format('Y-m-d');
+        });
     }
 }
